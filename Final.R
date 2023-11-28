6259b8b91723b16e61848714a431a118fa9245db
zchen-66 <andrewzitongchen@gmail.com>
2023-11-27 16:19:42 -0800

    data wrangled

--- Final.R
+++ Final.R
@@ -1,7 +1,32 @@
+# Import needed libraries
 library(dplyr)
 library(stringr)
 library(ggplot2)
 
+# Create dataframes
 air_df <- read.csv("AirQuality.csv")
 air_df <- filter(air_df, country_name == "United States of America")
-deaths_df <- read.csv("CountsOfDeaths.csv")
+deaths_df <- read.csv("DeathCounts.csv") 
+
+# Create state name columns for merging
+air_df <- filter(air_df, !is.na(city))
+air_df$state_code <- str_sub(air_df$"city", str_locate(air_df[, "city"], ",")[,1]+2, 
+                             str_locate(air_df[, "city"], ",")[,1]+3)
+air_df$state_name <- state.name[match(air_df$"state_code", state.abb)]
+
+deaths_df$state_name <- state.name[match(deaths_df$"STATE", state.abb)]
+
+# Aggregate air dataframe by finding the mean air quality concentration for 
+# each year for each state
+air_df <- group_by(air_df, year, state_name)
+new_air_df <- summarize(
+  air_df,
+  avg_pm10 = mean(pm10_concentration, na.rm=TRUE),
+  avg_pm25 = mean(pm25_concentration, na.rm=TRUE),
+  avg_no2 = mean(no2_concentration, na.rm=TRUE)
+)
+
+# Merge dataframe
+df <- merge(x=deaths_df, y=new_air_df, by.x=c("state_name", "YEAR"), by.y=c("state_name", "year"), all.x=TRUE)
+df <- subset(df, select=c(-URL))
+df <- filter(df, YEAR != 2005)

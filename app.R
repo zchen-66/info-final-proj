ae50a3776d46fd47d4e63cd70e79669fc39e8cd2
zchen-66 <andrewzitongchen@gmail.com>
2023-12-06 13:16:37 -0800

    added app.r

---
+++ app.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(ggplot)
+
+# Define UI ----
+ui <- fluidPage(
+ 
+)
+
+# Define server logic ----
+server <- function(input, output){
+  
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
